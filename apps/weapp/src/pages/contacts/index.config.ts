ac616d785edcd827b137a0d9d089274820f75e1b
kente0804 <bxw.cookies@gmail.com>
2026-05-16 12:50:04 +0800

    feat: 优化主题

--- apps/weapp/src/pages/contacts/index.config.ts
+++ apps/weapp/src/pages/contacts/index.config.ts
@@ -1,5 +1,9 @@
 export default {
   navigationStyle: 'custom',
+  navigationBarBackgroundColor: '#ffffff',
+  navigationBarTextStyle: 'black',
+  backgroundColor: '#ffffff',
+  backgroundColorTop: '#ffffff',
   usingComponents: {},
   disableScroll: true,
 }
