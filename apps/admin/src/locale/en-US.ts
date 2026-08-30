0e0fe38c51bf7a10b0cd30496a80d3d80118858f
zhangyi <465782548@qq.com>
2026-08-30 21:10:03 +0800

    chore: trigger admin_web rebuild

--- apps/admin/src/locale/en-US.ts
+++ apps/admin/src/locale/en-US.ts
@@ -6,6 +6,7 @@ import localeWorkplace from '@/views/dashboard/workplace/locale/en-US';
 import localeSettings from './en-US/settings';
 
 export default {
+  // Admin sidebar menu labels
   'menu.dashboard': 'Workplace',
   'menu.server.dashboard': 'Dashboard-Server',
   'menu.server.workplace': 'Workplace-Server',
